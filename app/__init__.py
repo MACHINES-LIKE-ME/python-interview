35b2e8c815df5c7c01610051e5458c80f139d515
SantiagoLosa <santiago.losa@machineslikeme.com>
2024-12-09 21:03:27 -0300

    fix template

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,12 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-
-    @app.route("/")
-    def hello_world():
-        return "Hello, World!"
-
+    
+    # Configurations
+    app.config['DEBUG'] = True
+    
+    # Register blueprints/routes
+    from .routes import bp
+    app.register_blueprint(bp)
+    
     return app
-
-# This `app` will be imported by `main.py` or by the Flask CLI.
-app = create_app()
--- app/main.py
+++
@@ -1,6 +0,0 @@
-from app import app
-
-if __name__ == "__main__":
-    # Running Flask's development server directly:
-    # When using `flask run`, the environment variable FLASK_APP=app/main.py will be used.
-    app.run(host="0.0.0.0", port=5000, debug=True)
